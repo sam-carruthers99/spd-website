e96a8726b8beead7363f3b68be8629aca860d864
sam-carruthers99 <samcarruthers99@gmail.com>
2024-09-10 02:43:01 -0600

    changing out riddles DB for JSON file

--- src/Riddles.js
+++ src/Riddles.js
@@ -4,52 +4,14 @@ import { useEffect, useState } from 'react';
 import AWS from 'aws-sdk';
 
 export default function Riddles() {
+    
     const [cards, setCards] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const cardsPerPage = 10;
 
-    const key = process.env.REACT_APP_ACCESS_KEY; // react requires REACT_APP_ prefix on an env variable to load it properly
-    const secret_key = process.env.REACT_APP_SECRET_ACCESS_KEY;
-
-    const client = new AWS.DynamoDB.DocumentClient({
-        region: 'us-east-2',
-        accessKeyId: key,
-        secretAccessKey: secret_key
-    });
-
-    const params = {
-        TableName: 'riddles'
-    };
-
-
     // TO DO
-    // Create a JSON file that has all the data for riddles that you can add to
-    // Uploads the riddles to the database in AWS, and can add stuff to it then run the script once each time 
-
-    useEffect(() => {
-        const scanTable = async () => {
-            try {
-                let items = [];
-                let data;
-                do {
-                    data = await client.scan(params).promise();
-                    items = items.concat(data.Items);
-                    params.ExclusiveStartKey = data.LastEvaluatedKey;
-                } while (typeof data.LastEvaluatedKey !== "undefined");
-
-                const newCards = items.map((item, index) => {
-                    const { answer, title, riddle } = item;
-                    return <Card key={index} title={title} riddle={riddle} answer={answer} />;
-                });
-                
-                setCards(newCards);
-            } catch (err) {
-                console.error('Error fetching data:', err);
-            }
-        };
-
-        scanTable();
-    }, []); // Empty dependency array ensures this runs only once
+    // Create a JSON file that has all the data for riddles that you can add to, then read that in here rather than DB
+    //  <Card key={index} title={title} riddle={riddle} answer={answer} />;
 
     // Get current cards
     const indexOfLastCard = currentPage * cardsPerPage;
